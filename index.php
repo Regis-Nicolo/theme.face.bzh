18e9eb895511cd2897e022449efdfa4f67ca87ba
[regis.nicolo@hotmail.frgit config --global user.name[Regis <regis.nicolo@hotmail.fr>
2018-05-25 17:12:31 +0200

    Création des premiers fichiers PHP

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis, urna">
+<?php wp_head(); ?>
+</head>
+
+<body>
+	<h1>Face</h1>
+
+		<p> 	
+
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis, urna non accumsan pellentesque, lacus justo consequat risus, nec fermentum sem nulla ac sapien. Maecenas pharetra urna et elit sodales, vitae vestibulum massa porta. Donec id elit neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis, urna non accumsan pellentesque, lacus justo consequat risus, nec fermentum sem nulla ac sapien. Maecenas pharetra urna et elit sodales, vitae vestibulum massa porta. Donec id elit neque.
+		</p>
+			<div class="intro presentation-row row">
+		<div class="presentation-text col-xs-12 col-md-4">
+
+			<img class="img-fluid"  src= "assets/images/environnement.jpg" alt="">
+			<img class="img-fluid" src="assets/images/logo.png"
+			  alt="">
+		</div>
+		<div class="introFACE presentation-text col-xs-12 col-md-8">
+			<h1> Fondation Agir contre l'exclusion<br>(fondation.org)</h1>
+				<p>	
+
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis, urna non accumsan pellentesque, lacus justo consequat risus, nec fermentum sem nulla ac sapien. Maecenas pharetra urna et elit sodales, vitae vestibulum massa porta. Donec id elit neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis, urna non accumsan pellentesque, lacus justo consequat risus, nec fermentum sem nulla ac sapien. Maecenas pharetra urna et elit sodales, vitae vestibulum massa porta. Donec id elit neque.
+				</p>
+			</div>
+		</div>
+
+	<hr class="ligne" />
+	<h2>&Agrave; la une</h2>
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
