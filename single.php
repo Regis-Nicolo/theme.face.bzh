6e1c847556b20db2205bd9c2c7ab875b56063823
Erwann duclos <codeacademie35@gmail.com>
2018-06-13 17:06:22 +0200

    Styling single

--- single.php
+++ single.php
@@ -9,9 +9,12 @@ Template Name: Page d'archives
         <div class='col-lg-8 presentation-text'>
             <h3><?php the_title() ?></h3>
             <hr class='ligne'>
-            <div style='text-align:center'>
-            <?php the_post_thumbnail(); ?>
-            <?php the_content(); ?>
+            <div>
+
+                <div style='text-align: center'>
+                    <?php the_post_thumbnail('medium', ['class' => 'img-responsive responsive--full']); ?>
+                </div>
+                <?php the_content(); ?>
             </div>
             <div class='footer'>
                
@@ -30,8 +33,47 @@ Template Name: Page d'archives
             
             
         </div>
-        <div class='col-lg-3 offset-lg-1 presentation-text'>
-            
+        <div class='col-lg-3 offset-lg-1 presentation-text aside'>
+            <?php
+                // Default arguments
+                $args = array(
+                    'posts_per_page' => 4, // How many items to display
+                    'post__not_in'   => array( get_the_ID() ), // Exclude current post
+                    'no_found_rows'  => true, // We don't ned pagination so this speeds up the query
+                );
+
+                // Check for current post category and add tax_query to the query arguments
+                $cats = wp_get_post_terms( get_the_ID(), 'category' ); 
+                $cats_ids = array();  
+                foreach( $cats as $wpex_related_cat ) {
+                    $cats_ids[] = $wpex_related_cat->term_id; 
+                }
+                if ( ! empty( $cats_ids ) ) {
+                    $args['category__in'] = $cats_ids;
+                }
+
+                // Query posts
+                $wpex_query = new wp_query( $args );
+
+                // Loop through posts
+                foreach( $wpex_query->posts as $post ) : setup_postdata( $post ); ?>
+                    <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>">
+                        <div class='block'>
+                            
+                            <?php the_post_thumbnail('thumbnail', ['class' => 'img-responsive responsive--full']); ?>
+                            <div class='block-title'>
+                             <?php the_title(); ?>
+                            </div>
+
+                        </div>
+                    </a>
+
+                <?php
+                // End loop
+                endforeach;
+
+                // Reset post data
+                wp_reset_postdata(); ?>
         </div>
 
     </div>
