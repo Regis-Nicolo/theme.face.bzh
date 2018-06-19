e505986de1bf412d9a226c998d90f0a8b85e7aa6
Unknown <codeacademie35@gmail.com>
2018-06-19 15:11:35 +0200

    Removing CDN links

--- functions.php
+++ functions.php
@@ -9,7 +9,8 @@ function Rnface_scripts() {
 	wp_enqueue_style('Rnface_fontawesome-core',
 		get_template_directory_uri() . '/css/fontawesome-all.min.css', array(), RNFACE_VERSION,'all');
 	wp_enqueue_style('Rnface_custom', get_template_directory_uri() . '/style.css', array('Rnface_bootstrap-core'), RNFACE_VERSION, 'all');	
-	wp_enqueue_script('Rnface_admin_script', get_template_directory_uri() . '/js/face.js', array('jquery'), RNFACE_VERSION, true);
+	wp_enqueue_script('jquery_script', get_template_directory_uri() . '/js/jquery-3.3.1.slim.min.js', array('jquery'), RNFACE_VERSION, true);
+	wp_enqueue_script('Rnface_admin_script', get_template_directory_uri() . '/js/face.js', array('face'), RNFACE_VERSION, true);
 
 }
 add_action('wp_enqueue_scripts', 'Rnface_scripts');
